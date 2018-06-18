b0af6e123bb5daf206ae0946f0dfed899dea9be5
Gilles MIFSUD <gilles.mifsud@netapsys.fr>
2018-06-18 08:20:02 +0200

    Updated latest babel

--- scripts.sh
+++ scripts.sh
@@ -4,5 +4,8 @@ yarn global add babel-cli
 yarn init
 yarn add babel-preset-react
 yarn add babel-preset-env
-babel src/app.js --out-file=public/scripts/app.js --presets=env,react --watch
+
+OLD : babel src/app.js --out-file=public/scripts/app.js --presets=env,react --watch
+NEW : npx babel src/app.js --out-file=public/scripts/app.js --presets=env,react --watch
+
 babel src/playground/es6-classes-1.js --out-file=public/scripts/app.js --presets=env,react --watch
