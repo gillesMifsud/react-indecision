26676f69ce2a25ba2bf9461827fba21827f0f20f
gillesMifsud <gil.mifsud@gmail.com>
2018-06-26 13:02:42 +0200

    6-57 Done

--- scripts.sh
+++ scripts.sh
@@ -7,8 +7,7 @@ yarn add babel-preset-env
 
 OLD : babel src/app.js --out-file=public/scripts/app.js --presets=env,react --watch
 NEW : npx babel src/app.js --out-file=public/scripts/app.js --presets=env,react --watch
-
 babel src/playground/es6-classes-1.js --out-file=public/scripts/app.js --presets=env,react --watch
 
-yarn run serve
-yarn run build
+yarn run dev-server : local dev, webpack in charge of bundle.js in memory
+yarn run build : production, generating a physical bundle.js file with our app
